8f61dc8dd3c52b65a81d3a3ec3dcfdc668ee549b
fauzan.lisan <fauzan.lisan@solusi247.com>
2026-01-14 14:42:36 +0700

    update seeder

--- seed.py
+++ seed.py
@@ -10,10 +10,29 @@ def seed_data():
     db: Session = SessionLocal()
 
     perms_data = [
-        {"name": "item:view", "desc": "Bisa melihat daftar barang"},
+        {"name": "item:view_all", "desc": "Bisa melihat daftar barang"},
+        {"name": "item:view", "desc": "Bisa melihat satu barang"},
         {"name": "item:create", "desc": "Bisa menambah barang baru"},
         {"name": "item:update", "desc": "Bisa mengedit barang"},
         {"name": "item:delete", "desc": "Bisa menghapus barang"},
+        {"name": "permission:view_all", "desc": "Bisa melihat daftar permission"},
+        {"name": "permission:view", "desc": "Bisa melihat satu permission"},
+        {"name": "permission:create", "desc": "Bisa menambah permission baru"},
+        {"name": "permission:update", "desc": "Bisa mengedit permission"},
+        {"name": "permission:delete", "desc": "Bisa menghapus permission"},
+        {"name": "role:view_all", "desc": "Bisa melihat daftar role"},
+        {"name": "role:view", "desc": "Bisa melihat satu role"},
+        {"name": "role:create", "desc": "Bisa menambah role baru"},
+        {"name": "role:update", "desc": "Bisa mengedit role"},
+        {"name": "role:delete", "desc": "Bisa menghapus role"},
+        {"name": "role:assign", "desc": "Bisa menambahkan role ke user"},
+        {"name": "role:add_permission", "desc": "Bisa menambahkan permission ke role"},
+        {"name": "role:remove_permission", "desc": "Bisa menghapus permission dari role"},
+        {"name": "user:view_all", "desc": "Bisa melihat daftar user"},
+        {"name": "user:view", "desc": "Bisa melihat satu user"},
+        {"name": "user:create", "desc": "Bisa menambah user baru"},
+        {"name": "user:update", "desc": "Bisa mengedit user"},
+        {"name": "user:delete", "desc": "Bisa menghapus user"},
     ]
 
     perms_obj = {}
@@ -35,7 +54,13 @@ def seed_data():
     staff_role = db.query(Role).filter(Role.name == "STAFF").first()
     if not staff_role:
         staff_role = Role(name="STAFF", description="Storage staff")
-        staff_role.permissions = [perms_obj["item:view"], perms_obj["item:create"]]
+        staff_role.permissions = [
+            perms_obj["item:view_all"],
+            perms_obj["item:view"], 
+            perms_obj["item:create"],
+            perms_obj["user:view"],
+            perms_obj["user:update"]
+        ]
         db.add(staff_role)
     
     db.commit()
